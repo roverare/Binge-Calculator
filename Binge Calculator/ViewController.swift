d22f1a40f69b14311f8c634dd2f801f16d0022c4
roverare <roverare@gmail.com>
2020-04-30 16:07:13 -0400

    Initial Commit

---
+++ Binge Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binge Calculator
+//
+//  Created by Rebecca Overare on 4/30/20.
+//  Copyright © 2020 Rebecca Overare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
